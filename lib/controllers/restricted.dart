ab42e0a868e9aa6cdca7d91fb857518800fa9a23
suragch <studymongolian@gmail.com>
2019-04-15 17:49:07 -0700

    password hashing

--- lib/controllers/restricted.dart
+++ lib/controllers/restricted.dart
@@ -1,20 +1,20 @@
 import 'dart:async';
 import 'dart:convert';
+import 'package:crypto/crypto.dart';
 import 'package:aqueduct/aqueduct.dart';
 import 'package:dart_auth/helpers/user.dart';
 import 'package:dart_auth/helpers/database.dart';
 
 class RestrictedController extends ResourceController {
-
   @Operation.get()
-  Future<Response> restricted(@Bind.header("authorization") String authHeader) async {
-
+  Future<Response> restricted(
+      @Bind.header("authorization") String authHeader) async {
     // only allow with correct username and password
     if (!_isAuthorized(authHeader)) {
       return Response.forbidden();
     }
 
-    // We are returning a string here, but this could be 
+    // We are returning a string here, but this could be
     // a file or data from the database.
     return Response.ok('restricted resource');
   }
@@ -24,7 +24,7 @@ class RestrictedController extends ResourceController {
     final parts = authHeader.split(' ');
     if (parts == null || parts.length != 2 || parts[0] != 'Basic') {
       return false;
-    } 
+    }
     return _isValidUsernameAndPassword(parts[1]);
   }
 
@@ -40,8 +40,21 @@ class RestrictedController extends ResourceController {
     final User foundUser = database.queryEmail(user.email);
 
     // check for match
-    return foundUser != null && 
-      foundUser.password == user.password;
+    return foundUser != null &&
+        _passwordHashMatches(foundUser.password, user.password);
   }
 
+  bool _passwordHashMatches(String saltHash, String password) {
+    // previously saved password hash
+    final parts = saltHash.split('.');
+    final salt = parts[0];
+    final savedHash = parts[1];
+
+    // user submitted password hash
+    final saltedPassword = salt + password;
+    final bytes = utf8.encode(saltedPassword);
+    final newHash = sha256.convert(bytes).toString();
+
+    return savedHash == newHash;
+  }
 }
--- lib/controllers/signup.dart
+++ lib/controllers/signup.dart
@@ -1,10 +1,10 @@
 import 'dart:async';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
 import 'package:aqueduct/aqueduct.dart';
 import 'package:dart_auth/helpers/user.dart';
 import 'package:dart_auth/helpers/database.dart';
 import 'package:string_validator/string_validator.dart';
-import 'package:crypto/crypto.dart';
-import 'dart:convert';
 
 class SignupController extends ResourceController {
   @Operation.post()
@@ -26,7 +26,7 @@ class SignupController extends ResourceController {
     }
 
     // salt and hash the password
-    final hash = _hashPassword(user.password);
+    user.password = _hashPassword(user.password);
 
     // add user to database
     user.id = database.addUser(user);
@@ -49,10 +49,11 @@ class SignupController extends ResourceController {
   }
 
   String _hashPassword(String password) {
-    var password = 'password123';
-var salt = 'UVocjgjgXg8P7zIsC93kKlRU8sPbTBhsAMFLnLUPDRYFIWAk';
-var saltedPassword = salt + password;
-var bytes = utf8.encode(saltedPassword);
-var hash = sha256.convert(bytes);
+    final salt = AuthUtility.generateRandomSalt();
+    final saltedPassword = salt + password;
+    final bytes = utf8.encode(saltedPassword);
+    final hash = sha256.convert(bytes);
+    // store the salt with the hash
+    return '$salt.$hash';
   }
 }
